e282531cc7a81aca8e8c1bb3574ae68985bc5733
John B An <jan10@ugrad1.cs.jhu.edu>
2014-11-18 16:15:57 -0500

    linked list code compiles, needs review

--- linked_list.c
+++ linked_list.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_MESSAGE_SIZE 80
 
 typedef struct like_node {
-    int lamport_counter;
-    int service_index;
+    int timestamp;
+    int server_index;
     char * username;
     struct like_node * next;
 } like_node;
@@ -32,7 +33,7 @@ typedef struct lamport_timestamp {
 
 int add_chatroom(char * new_name);
 int add_message(char * new_mess, char * room_name, lamport_timestamp ts);
-int add_like(message_node * mess, like_node * new_like, lamport_timestamp ts);
+int add_like(char * user, message_node * mess, lamport_timestamp ts);
 
 struct chatroom_node * chatroom_head;
 
@@ -56,9 +57,28 @@ int add_chatroom(char * new_name) {
     }
 
     chatroom_node * curr = chatroom_head;
-    //else find next null, add to end
-    while (curr->next != NULL) {
+
+    /*
+     * check for duplicates
+    while (curr == NULL) {
+        if (strcmp(curr->chatroom_name, new_name))
+            return 0;
         curr = curr->next;
+    }
+    curr = chatroom_head;
+     */
+
+    //else find next null, add to end, return if duplicate found
+    while (curr->next != NULL) {
+        if (strcmp(curr->chatroom_name, new_name) == 0) {
+            return 0;
+        }
+        curr = curr->next;
+    }
+
+    //TODO fix hack to find duplicate
+    if (strcmp(curr->chatroom_name, new_name) == 0) {
+        return 0;
     }
 
     curr->next = to_add;
@@ -66,21 +86,18 @@ int add_chatroom(char * new_name) {
     return 1;
 }
 
-/*FIXME IS A MESS*/
 int add_message(char * new_mess, char * room_name, lamport_timestamp ts) {
 
     struct chatroom_node * curr_room = chatroom_head;
-    struct chatroom_node * prev_room;
     struct message_node * curr_mess;
+    int lamport = (10 * ts.timestamp) + (ts.server_index);
+
+    /*make chatroom guaranteed to exist*/
+    add_chatroom(room_name);
 
     /*look for correct chatroom*/
-    while (curr_room != NULL && strcmp(curr_room->chatroom_name, room_name) == 0) {
-        prev_room = curr_room;
+    while (strcmp(curr_room->chatroom_name, room_name) == 0) {
         curr_room = curr_room->next;
-    }
-
-    if (curr == NULL) {
-        add_chatroom(room_name);
     }
 
     /*create message to add*/
@@ -89,20 +106,84 @@ int add_message(char * new_mess, char * room_name, lamport_timestamp ts) {
     to_add->timestamp = ts.timestamp;
     to_add->chatroom_name = room_name;
 
+    /*iterate through messages in room*/
     curr_mess = curr_room->mess_head;
+
+    /*if head doesn't exist add, return*/
     if (curr_mess == NULL) {
-        prev_room->next = to_add;
+        curr_room->mess_head = to_add;
         return 1;
     }
 
-    while (curr_mess->next != NULL) {
-        curr_mess = curr_mess->next;
+    int curr_lamport = (10 * curr_mess->timestamp) + (curr_mess->server_index);
+
+    if (lamport < curr_lamport) {
+        to_add->next = curr_room->mess_head;
+        curr_room->mess_head = to_add;
+        return 1;
     }
 
+    curr_lamport = (10 * curr_mess->next->timestamp) + (curr_mess->next->server_index);
+
+    while (lamport > curr_lamport && curr_mess->next != NULL) {
+        curr_mess = curr_mess->next;
+        curr_lamport = (10 * curr_mess->next->timestamp) + (curr_mess->next->server_index);
+    }
+
+    if (curr_mess->next == NULL) {
+        curr_mess->next = to_add;
+        return 1;
+    }
+    to_add->next = curr_mess->next;
     curr_mess->next = to_add;
     return 1;
+
 }
 
-int add_like(message_node * mess, like_node * new_like, lamport_timestamp ts) {
-    return 0;
+int add_like(char * user, message_node * mess, lamport_timestamp ts) {
+    like_node * curr_like = mess->like_head;
+    int lamport = (10 * ts.timestamp) + (ts.server_index);
+    
+    like_node * to_add = malloc(sizeof(like_node));
+    to_add->server_index = ts.server_index;
+    to_add->timestamp = ts.timestamp;
+    strcpy(to_add->username, user);
+
+    if (curr_like == NULL) {
+        mess->like_head = to_add;
+        return 1;
+    }
+
+    /*check for duplicates*/
+    while (curr_like != NULL) {
+        if (strcmp(curr_like->username, user)) {
+            return 0;
+        }
+        curr_like = curr_like->next;
+    }
+    curr_like = mess->like_head;
+
+    /*insert by lamport*/
+    int curr_lamport = (10 * curr_like->timestamp) + (curr_like->server_index);
+
+    if (lamport < curr_lamport) {
+        to_add->next = mess->like_head;
+        mess->like_head = to_add;
+        return 1;
+    }
+
+    curr_lamport = (10 * curr_like->next->timestamp) + (curr_like->next->server_index);
+
+    while (lamport > curr_lamport && curr_like->next != NULL) {
+        curr_like = curr_like->next;
+        curr_lamport = (10 * curr_like->next->timestamp) + (curr_like->next->server_index);
+    }
+
+    if (curr_like->next == NULL) {
+        curr_like->next = to_add;
+        return 1;
+    }
+    to_add->next = curr_like->next;
+    curr_like->next = to_add;
+    return 1;
 }
