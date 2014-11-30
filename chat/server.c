ca377870139168bb03fe2e3dae919b5529a71b4d
Edward <eschemb1@jhu.edu>
2014-11-30 16:30:06 -0500

    Entropy matrix sending fixes

--- chat/server.c
+++ chat/server.c
@@ -39,6 +39,7 @@ static char      server_group[MAX_NAME] = SERVER_GROUP_NAME;
 static char      User[MAX_NAME] = "1";
 static mailbox   Mbox;
 int              ret;
+int              num_groups;
 
 int              entropy_matrix[NUM_SERVERS][NUM_SERVERS];
 int              entropy_received = 0;
@@ -46,7 +47,7 @@ int              entropy_received = 0;
 int              lamport_counter;
 
 struct chatroom_node    head; //The main data structure
-update_array     updates[NUM_SERVERS]; //Update struct for each server
+update_array            updates[NUM_SERVERS]; //Update struct for each server
 
 
 int main(int argc, char *argv[])
@@ -69,6 +70,12 @@ int main(int argc, char *argv[])
 	ret = SP_join(Mbox, server_group);
 	if(ret < 0) SP_error(ret);
 
+	/** Initialize the udpate arrays **/
+	for(int i = 0; i < NUM_SERVERS; i++) {
+		updates[i].size = INITIAL_SIZE;
+		updates[i].array = malloc(sizeof(update)*INITIAL_SIZE);
+	}
+
 	/* Process and send messages using the Spread Event System */
 	E_init();
 
@@ -87,7 +94,6 @@ static void Handle_messages()
 	int     endian_mismatch;
 	int     service_type;
 	char    sender[MAX_GROUP_NAME];
-	int     num_groups;
 	int16   mess_type;
 	char    mess[1500];
 	static char group[80];
@@ -97,7 +103,10 @@ static void Handle_messages()
 		target_groups, &mess_type, &endian_mismatch, sizeof(update), mess);
 	
 	if(Is_regular_mess( service_type )) {
-		//received_update = (received_update) mess;
+		//Cast the message to an update
+		received_update = *((update *) mess);
+
+		/** Check if it is an unlike **/
 		if(received_update.type == -1) {
 			//Perform the unlike update on linked list
 			//TODO - Need data structure
@@ -113,16 +122,18 @@ static void Handle_messages()
 			Compare_Lamport(received_update.lamport.timestamp);
 
 			//Increase entropy vector
-			if(received_update.lamport.timestamp > entropy_matrix[machine_index][origin]) {
-				entropy_matrix[machine_index][origin] = received_update.lamport.timestamp;
+			if(received_update.lamport.timestamp > entropy_matrix[machine_index-1][origin]) {
+				entropy_matrix[machine_index-1][origin] = received_update.lamport.timestamp;
 			}
 
 			//Multicast the update to all servers if the update was from a client
 			if(strcmp(target_groups[0], SERVER_GROUP_NAME) != 0) {
 				SP_multicast(Mbox, AGREED_MESS, server_group, 1, MAX_MESSLEN,
-					(char *) received_update);
+					(char *) &received_update);
 			}
 		}
+
+		/** Check if it is a like  **/
 		else if(received_update.type == 1) {
 			//Perform the like update
 			//TODO - Need data structure
@@ -138,16 +149,18 @@ static void Handle_messages()
 			Compare_Lamport(received_update.lamport.timestamp);
 
 			//Increase entropy vector	
-			if(received_update.lamport.timestamp > entropy_matrix[machine_index][origin]) {
-				entropy_matrix[machine_index][origin] = received_update.lamport.timestamp;
+			if(received_update.lamport.timestamp > entropy_matrix[machine_index-1][origin]) {
+				entropy_matrix[machine_index-1][origin] = received_update.lamport.timestamp;
 			}
 
 			//Multicast the update to all servers
 			if(strcmp(target_groups[0], SERVER_GROUP_NAME) != 0) {
 				SP_multicast(Mbox, AGREED_MESS, server_group, 1, MAX_MESSLEN,
-					(char *) received_update);
+					(char *) &received_update);
 			}
 		}
+
+		/** Check if it is a chat message **/
 		else if(received_update.type == 0) {
 			//Perform the new message update
 			//TODO - Need data structure
@@ -163,27 +176,35 @@ static void Handle_messages()
 			Compare_Lamport(received_update.lamport.timestamp);
 
 			//Increase entropy vector
-			if(received_update.lamport.timestamp > entropy_matrix[machine_index][origin]) {
-				entropy_matrix[machine_index][origin] = received_update.lamport.timestamp;
+			if(received_update.lamport.timestamp > entropy_matrix[machine_index-1][origin]) {
+				entropy_matrix[machine_index-1][origin] = received_update.lamport.timestamp;
 			}
 
 			//Multicast the update to all servers
 			if(strcmp(target_groups[0], SERVER_GROUP_NAME) != 0) {
 				SP_multicast(Mbox, AGREED_MESS, server_group, 1, MAX_MESSLEN,
-					(char *) received_update);
+					(char *) &received_update);
 			}
 		}
+
+		/** Check if it is an entropy vector for merging **/
 		else if(received_update.type == 2) {
-			//If all have been received, send out updates if you are the max
-			//TODO: Deal with mid partition - ie) look for membership change
-			entropy_received++;
-			//TODO: update matrix based on new vector
-			/*if(entropy_received = num in group) {
-				Send_Merge_Updates();
-			}*/
-			
+			//Update your matrix with the received message
+			int origin = received_update.lamport.server_index;
 			for(int i = 0; i < NUM_SERVERS; i++) {
-				entropy_matrix[machine_index][i] = entropy_matrix[i][i];
+				entropy_matrix[origin][i] = received_update.vector[i];
+			}
+
+			//TODO: Deal with mid partition - ie) look for membership change
+			
+			//If all have been received, send out updates if you are the max
+			if(++entropy_received == num_groups) {
+				Send_Merge_Updates();
+			}
+			
+			//Update your local vector
+			for(int i = 0; i < NUM_SERVERS; i++) {
+				entropy_matrix[machine_index-1][i] = entropy_matrix[i][i];
 			}
 
 			Clear_Updates();
@@ -191,35 +212,35 @@ static void Handle_messages()
 			
 	}else if( Is_membership_mess( service_type )) {
 		
-		//Check if it was an update from the server group
+		/** Check if it was an update from the server group **/
 		if(strcmp(target_groups[0], SERVER_GROUP_NAME) == 0) {
 			//If it was an addition, merge
 			if(Is_caused_join_mess(service_type)) {
-				//Send your anti-entropy vector
+				//Send your anti-entropy vector as an update
+				update entropy_update = malloc(sizeof(update));
+				for(int i = 0; i < NUM_SERVERS; i++) {
+					entropy_update.vector[i] = entropy_matrix[machine_index-1][i];
+				}
 				SP_multicast(Mbox, AGREED_MESS, group, 1, MAX_MESSLEN, 
-					(char *) entropy_matrix[machine_index]);
+					(char *) &update);
 				entropy_received = 0;
 			}
-		}
 
-		//Check if it was an update from a chatroom group
-		//TODO - fix loop and if - Need data structure
-		for (int i = 0; i < 0; i++) {
-			if(strcmp(target_groups[0], "Test") == 0) {
-				//Update your users list
-				//TODO - Need data structure
+		/** The update came from a chatroom group **/
+		}else{
+			//Update your users list
+			//TODO - Need data structure
 				
-				//Multicast the update to all servers
-				SP_multicast(Mbox, AGREED_MESS, server_group, 1, MAX_MESSLEN,
-					(char *) received_update);	
-			}
+			//Multicast the update to all servers
+			//TODO - Create a "<user> joined" or "<user> left" update
+			//TODO - Multicast the update
 		}
 	}
 }
 
 void Send_Merge_Updates()
 {
-	/** Should it be max in group? **/
+	/** TODO: Should it be max in group? **/
 	/** Send necessary updates **/
 	for(int i = 0; i < NUM_SERVERS; i++) {
 		if(Is_Max(entropy_matrix[i])) {
@@ -229,6 +250,8 @@ void Send_Merge_Updates()
 				int s = updates[i].size;
 				while(updates[i].array[current%s].lamport.timestamp < entropy_matrix[i][j]) {
 					//Send the update to the machines which need it
+					//TODO - Need to re-think this method
+
 					//send(updates[i][current%s]; -> Use private group
 					current++;
 				}
@@ -246,7 +269,7 @@ int Is_Max(int vector[])
 		if(vector[i] > server_val)
 			return 0;
 		if(vector[i] == server_val) {
-			if(i > machine_index)
+			if(i > machine_index-1)
 				return 0;
 		}
 	}
@@ -266,17 +289,23 @@ int Min_Val(int vector[])
 
 void Send_All_Messages()
 {
+	/** Sends all the messages it has from a particular chatroom to a specific client
+	 * connected to the server and in that chatroom **/
+	
 	//TODO - Need data structure
 }
 
 void Send_Recent_TwentyFive()
 {
+	/** Sends the most recent 25 messages it has from a particular chatroom to a specific
+	 * client connected to the server and in that chatroom **/
+
 	//TODO - Need data structure
 }
 
 void Clear_Updates()
 {
-	/** Sets updates which are no longer needed to NULL in the udpates array**/
+	/** Sets updates which are no longer needed to type = -2 in the udpates array**/
 	int min = 0;
 	int current;
 	int size;
@@ -293,6 +322,8 @@ void Clear_Updates()
 
 void Compare_Lamport(int lamport)
 {
+	/** Compares a received lamport timestamp to the server's - if the received one is higer, 
+	 * it sets the server's equal to it**/
 	if(lamport > lamport_counter) {
 		lamport_counter = lamport;
 	}
--- chat/update_array.c
+++ chat/update_array.c
@@ -1,18 +1,21 @@
+#define INITIAL_SIZE 10
+
 /** Doubling array for storing updates **/
 typedef struct update_array {
-	update array[10];
-	int size;
-	int element_count;
-	int start;
+	update *array; //Pointer to array to deal with re-sizing
+	int  size;
+	int  element_count;
+	int  start;
 } update_array;
 
 update_array attempt_double(update_array ua)
 {
-	/** Double when the array is full **/
+	/** Double if the array is full **/
 	if(ua.element_count == ua.size)
 	{
-		update expanded[2*ua.size];
-		ua.size = 2*ua.size;
+		update *expanded;
+		ua.size = ua.size*2;
+		expanded = malloc(sizeof(update)*ua.size);
 
 		for(int i = 0; i < ua.element_count; i++) {
 			expanded[i] = ua.array[i];
@@ -22,4 +25,5 @@ update_array attempt_double(update_array ua)
 	}
 
 	return ua;
+
 }
